811450d322fdd3ec7ecb2c0b8b3bfadd751ed38d
Waseem Maya <waseemmayaa@gmail.com>
2018-09-03 03:20:49 +0500

    push

--- All News Project/main.js
+++ All News Project/main.js
@@ -1,31 +1,31 @@
-// $(document).ready(function($) {
-//   $(".container--grid ul").mixItUp({
-//     animation: {
-//       duration: 350,
-//       effects: "fade translateY(40px)",
-//       easing: "ease"
-//     }
-//   });
-// });
+$(document).ready(function($) {
+  $(".container--grid ul").mixItUp({
+    animation: {
+      duration: 350,
+      effects: "fade translateY(40px)",
+      easing: "ease"
+    }
+  });
+});
 
-// $("a").on("touchstart", function(e) {
-//   "use strict";
-//   var link = $(this);
-//   if (link.hasClass("hover")) {
-//     return true;
-//   } else {
-//     link.addClass("hover");
-//     $("a")
-//       .not(this)
-//       .removeClass("hover");
-//     e.preventDefault();
-//     return false;
-//   }
-// });
+$("a").on("touchstart", function(e) {
+  "use strict";
+  var link = $(this);
+  if (link.hasClass("hover")) {
+    return true;
+  } else {
+    link.addClass("hover");
+    $("a")
+      .not(this)
+      .removeClass("hover");
+    e.preventDefault();
+    return false;
+  }
+});
 
-// if (!("ontouchstart" in document.documentElement)) {
-//   document.documentElement.className += "no-touch";
-// }
+if (!("ontouchstart" in document.documentElement)) {
+  document.documentElement.className += "no-touch";
+}
 
 function getDropDown() {
   let selector = document.getElementById("selector");
